d18b6ee840fa61ef83fbfe2b3d05d6ffc8cac663
felipedev1996 <felipepino1996@gmail.com>
2023-08-04 00:26:47 -0400

    config cors

--- src/config.js
+++ src/config.js
@@ -8,4 +8,4 @@ export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/react-mongo-auth-crud";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5174";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://frontend-react-auth-task.onrender.com/";
