756d5f2c5706023acb422c833def0051b2a49aec
felipedev1996 <felipepino1996@gmail.com>
2023-08-06 22:59:11 -0400

    error deploy render

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ config();
 export const PORT = process.env.PORT || 4000;
 
 
-export const MONGODB_URI = "mongodb://0.0.0.0:27017/react-mongo-auth-crud";
-// export const MONGODB_URI = process.env.MONGODB_URI ;
+// export const MONGODB_URI = "mongodb://0.0.0.0:27017/react-mongo-auth-crud";
+export const MONGODB_URI = process.env.MONGODB_URI ;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 
