7216ff90b646a250e4cf7d100a0e9748bac720cf
felipedev1996 <felipepino1996@gmail.com>
2023-08-15 02:00:26 -0400

    changes in enviroment variable index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import app from "./app.js";
-import { PORT } from "./config.js";
+import { FRONTEND_URL, PORT } from "./config.js";
 import { connectDB } from "./db.js";
 
 async function main() {
   try {
     await connectDB();
     app.listen(PORT);
-    console.log(`Listening on port http://localhost:${PORT}`);
+    console.log(`Listening on FRONTEND_URL: ${FRONTEND_URL}`);
     console.log(`Environm: ${process.env.NODE_ENV}`)
   } catch (error) {
     console.error(error);
