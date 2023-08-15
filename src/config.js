6ea8abed92f2ad34426232f701d5ac46a2b3f9ae
felipedev1996 <felipepino1996@gmail.com>
2023-08-15 19:00:30 -0400

    update dependencies

--- src/config.js
+++ src/config.js
@@ -5,12 +5,10 @@ config();
 export const PORT = process.env.PORT || 4000;
 
 
-// export const MONGODB_URI = "mongodb://0.0.0.0:27017/react-mongo-auth-crud";
-// export const MONGODB_URI = process.env.MONGODB_URI ;
 export const MONGODB_URI = "mongodb+srv://userimgur:pilipe123@imgur.l6fhfql.mongodb.net/react-task-auth?retryWrites=true&w=majority";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 
 
 // export const FRONTEND_URL = process.env.FRONTEND_URL || "https://white-ground-077ba4d0f.3.azurestaticapps.net"; 
-export const FRONTEND_URL =" http://127.0.0.1:5173" ; 
+export const FRONTEND_URL ="http://127.0.0.1:5174"; 
